7b01888eac212cf068d824dbcf3bcf4f4b560a2c
Wasin Phungwigrai <wasin.pwg@gmail.com>
2023-04-24 10:46:31 +0000

    feat: pandas

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import requests
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import time
-import aiohttp
 import asyncio
-from urllib.parse import urljoin, urldefrag, urlparse, unquote
+import aiohttp
+from urllib.parse import urljoin, urldefrag, urlparse, unquote, quote
 import urllib.robotparser
 from bs4 import BeautifulSoup
 import logging
 import json
 import tracemalloc
+import pandas as pd
+import encodings.idna
 
 
 class Parser:
+
     def __init__(self, text):
         self.soup = BeautifulSoup(text, 'html.parser')
 
@@ -25,16 +28,121 @@ class Parser:
         return urls
 
     def get_canonical_link(self):
-        return self.soup.find('link', {'rel': 'canonical'}).get('href')
+        element = self.soup.find('link', {'rel': 'canonical'})
+        if element is None:
+            return False
+        return element.get('href')
+
+
+class UrlParser:
+    """
+    A class for parsing URLs into their component parts.
+    """
+    @staticmethod
+    def get_root_url(url):
+        """
+        Given a URL, returns the root URL by extracting the scheme and netloc.
+
+        :param url: A string representing the URL to extract the root from.
+        :return: A string representing the root URL.
+        """
+        return urlparse(url).scheme + '://' + urlparse(url).netloc
+
+    @staticmethod
+    def encode_url_once(url):
+        """
+        Encodes the given URL only once, if it is not already encoded.
+
+        Args:
+            url (str): The URL to be encoded.
+
+        Returns:
+            str: The encoded URL.
+
+        """
+        if UrlParser().is_url_already_encoded(url):
+            return url
+        return UrlParser().encode_url(url)
+
+    @staticmethod
+    def encode_url(url):
+        """
+        Encodes a given URL by replacing non-ASCII characters in the domain component
+        with their IDNA encoding and encoding the path component while preserving the
+        path segments.
+
+        Args:
+            url (str): The URL to encode.
+
+        Returns:
+            str: The encoded URL.
+        """
+        url_components = urlparse(url)
+        domain = url_components.netloc
+        path = url_components.path
+
+        # Encode the non-ASCII characters in the domain component using the idna encoding method
+        encoded_domain = domain.encode('idna').decode()
+
+        # Encode the path component while preserving the path segments
+        encoded_path = "/".join(quote(segment) for segment in path.split("/"))
+
+        # Replace the domain and path components in the URL with the encoded versions
+        encoded_url = url.replace(
+            domain, encoded_domain).replace(path, encoded_path)
+        return encoded_url
+
+    @staticmethod
+    def decode_url(url):
+        """
+        Decodes a given URL by replacing Punycode-encoded domain and URL-encoded path components.
+
+        Args:
+            url (str): The URL to decode.
+
+        Returns:
+            str: The decoded URL.
+        """
+        url_components = urlparse(url)
+        domain = url_components.netloc
+        path = url_components.path
+
+        # Decode the Punycode-encoded domain component using the idna decoding method
+        decoded_domain = domain.encode().decode('idna')
+
+        # Decode the URL-encoded path component
+        decoded_path = "/".join(unquote(segment)
+                                for segment in path.split("/"))
+
+        # Replace the domain and path components in the URL with the decoded versions
+        decoded_url = url.replace(
+            domain, decoded_domain).replace(path, decoded_path)
+        return decoded_url
+
+    @staticmethod
+    def is_url_already_encoded(url):
+        """
+        Check if the given URL is already URL encoded.
+
+        Args:
+            url (str): The URL to check for encoding.
+
+        Returns:
+            bool: True if the URL is already encoded, False otherwise.
+        """
+        decoded_text = unquote(url)
+        if url == decoded_text:
+            return False
+        return True
 
 
 class Crawler:
-
     # format_processors = {
     #     'xml': XMLWriter,
     #     'txt': TextWriter
     # }
 
+    rp = {}
     exclude_urls = []
 
     def __init__(
@@ -53,15 +161,21 @@ class Crawler:
         :param maxtasks: maximum count of tasks. Default 100
         :type maxtasks: int
         """
-        self.url = url
+        self.url = UrlParser.encode_url_once(url)
         self.rooturl = f'{urlparse(url).scheme}://{urlparse(url).netloc}'
         self.todo_queue = todo_queue_backend()
         self.busy = set()
-        self.done = done_backend()
+        self.done = pd.DataFrame(columns=['url',
+                                          'url_encode',
+                                          'indexability',
+                                          'indexability_status',
+                                          'response_code',
+                                          'content_type',
+                                          'response_time',
+                                          'hash'])
         self.tasks = set()
         self.sem = asyncio.Semaphore(maxtasks)
         self.http_request_options = http_request_options or {}
-        self.rp = {self.rooturl: self.robots_txt()}
         self.links = list()
         self.errors = set()
         # connector stores cookies between requests and uses connection pool
@@ -69,9 +183,19 @@ class Crawler:
         self.limit = limit
         # self.writer = self.format_processors.get(out_format)(out_file)
 
-    def robots_txt(self):
+    def robots_txt(self, url):
+        """
+        Parses the `robots.txt` file for the given `rooturl` and returns a
+        `RobotFileParser` object.
+
+        :return: `RobotFileParser` object that was created after parsing
+                `robots.txt` file.
+        """
         rp = urllib.robotparser.RobotFileParser()
-        rp.set_url(urljoin(self.rooturl, 'robots.txt'))
+        response = requests.get(
+            urljoin(UrlParser.get_root_url(url), 'robots.txt'))
+        url = urljoin(UrlParser.get_root_url(response.url), 'robots.txt')
+        rp.set_url(url)
         rp.read()
         return rp
 
@@ -95,45 +219,45 @@ class Crawler:
             await asyncio.sleep(1)
         await t
         await self.session.close()
-        json_object = json.dumps(self.done, indent=4, ensure_ascii=False)
-        with open('json.json', 'w') as f:
-            f.write(json_object)
-        json_object = json.dumps(self.links, indent=4, ensure_ascii=False)
-        with open('json2.json', 'w') as f:
-            f.write(json_object)
+        self.done.to_excel('export.xlsx', index=False)
+        # json_object = json.dumps(self.done, indent=4, ensure_ascii=False)
+        # with open('json.json', 'w') as f:
+        #     f.write(json_object)
+        # json_object = json.dumps(self.links, indent=4, ensure_ascii=False)
+        # with open('json2.json', 'w') as f:
+        #     f.write(json_object)
 
         # await self.writer.write([key for key, value in self.done.items() if value])
 
     def check_allow_crawl(self, url):
         try:
-            if self.rp.get(self.get_root_url(url)) is None:
-                self.rp[self.get_root_url(url)] = self.robots_txt()
-            rp = self.rp.get(self.get_root_url(url))
+            if self.rp.get(UrlParser().get_root_url(url)) is None:
+                self.rp[UrlParser().get_root_url(url)] = self.robots_txt(url)
+            rp = self.rp.get(UrlParser().get_root_url(url))
             return rp.can_fetch('*', url)
         except Exception:
             return False
 
-    def get_root_url(self, url):
-        return urlparse(url).scheme + '://' + urlparse(url).netloc
-
-    async def addurls(self, urls):
+    async def addurls(self, urls, force=False):
         """
         Add urls in queue and run process to parse
         :param urls:
         :return:
         """
         for url, parenturl in urls:
+            url = UrlParser.encode_url_once(url)
+            parenturl = UrlParser.encode_url_once(parenturl)
             url = urllib.parse.urljoin(parenturl, url)
-            url, frag = urllib.parse.urldefrag(url)
+            url, frag = urldefrag(url)
             self.links.append([parenturl, url])
             if (parenturl.startswith(self.rooturl) and
                     not any(exclude_part in url for exclude_part in self.exclude_urls) and
                     url not in self.busy and
-                    url not in self.done and
+                    url not in self.done.url_encode and
                     url not in self.todo_queue and
-                    not self.is_done(url) and
                     (len(self.done) + len(self.busy) + len(self.todo_queue)
                      < self.limit or not self.limit)
+                    or force
                     ):
                 if self.check_allow_crawl(url):
                     self.todo_queue.add(url)
@@ -149,12 +273,8 @@ class Crawler:
                     self.tasks.add(task)
 
                 else:
-                    self.done[url] = {
-                        'url': url,
-                        'url_decode': unquote(url),
-                        'indexability': False,
-                        'indexability_status': 'Block by robots.txt'
-                    }
+                    self.append_done(
+                        url=url, indexability_status="Block by robots.txt")
 
     async def process(self, url):
         """
@@ -171,33 +291,26 @@ class Crawler:
 
         try:
             # await response
+            start = time.time()
             resp = await self.session.get(url, **self.http_request_options)
+            stop = time.time()
             for history in resp.history:
                 current_url = str(history.url)
-                self.done[current_url] = {
-                    "url": current_url,
-                    "url_decode": unquote(current_url),
-                    "indexability": False,
-                    "indexability_status": 'Redirected',
-                    "content_type": resp.headers.get('content-type'),
-                    "http_code": history.status
-                }
+                self.append_done(history, current_url,
+                                 indexability_status="Redirected")
             new_url = str(resp.url)
             # get response url
         except Exception as exc:
+            print(exc)
+            print(str(exc))
             # on any exception mark url as BAD
             print('...', url, 'has error', repr(str(exc)))
-            # if url not in self.errors:
-            #     self.errors.add(url)
-            #     self.busy.remove(url)
-            #     self.addurls(('', url))
-            # else:
-            self.done[url] = {
-                "url": url,
-                "url_decode": unquote(url),
-                "indexability": False,
-                "indexability_status": repr(str(exc))
-            }
+            if url not in self.errors:
+                self.errors.add(url)
+                asyncio.Task(self.addurls([('', url)], True))
+            else:
+                self.append_done(
+                    url=url, indexability_status=repr(str(exc)))
         else:
             # only url with status == 200 and content type == 'text/html' parsed
             if (resp.status == 200 and
@@ -206,26 +319,39 @@ class Crawler:
                 parser = Parser(data)
                 urls = parser.parse()
                 asyncio.Task(self.addurls([(u, url) for u in urls]))
+                self.append_done(resp, new_url, indexability=True,
+                                 hash_val=hash(data), response_time=stop-start)
+            else:
+                self.append_done(resp, new_url, indexability=True,
+                                 response_time=stop-start)
 
             # even if we have no exception, we can mark url as good
             resp.close()
-            self.done[new_url] = {
-                "url": new_url,
-                "url_decode": unquote(new_url),
-                "indexability": True,
-                "indexability_status": '',
-                "content_type": resp.headers.get('content-type'),
-                "http_code": resp.status
-            }
-        self.busy.remove(url)
-        # self.done[new_url]["time"] = stop - start
+
+        if url in self.busy:
+            self.busy.remove(url)
         logging.info(len(self.done), 'completed tasks,', len(self.tasks),
                      'still pending, todo_queue', len(self.todo_queue))
+
+    def append_done(self, response=None, url='', indexability=False, indexability_status=None, hash_val=None, response_time=None):
+        self.done = pd.concat([self.done, pd.DataFrame({
+            "url": UrlParser.decode_url(url),
+            "url_encode": UrlParser.encode_url_once(url),
+            "indexability": indexability,
+            "indexability_status": indexability_status,
+            "content_type": response.headers.get('content-type') if response else None,
+            "response_code": response.status if response else None,
+            "hash": hash_val,
+            "response_time": response_time
+        }, index=[0])])
 
     def set_exclude_url(self, urls_list):
         self.exclude_urls = urls_list
 
     def runsync(self):
+        """
+        Runs the `run` coroutine synchronously using an event loop.
+        """
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.run())
 
@@ -240,16 +366,21 @@ def check_duplicate(text1, text2):
 
 tracemalloc.start()
 c = Crawler('https://www.thailandpostmart.com/',
-            limit=1, http_request_options={"timeout": 60}, maxtasks=10)
+            limit=3, http_request_options={"timeout": 10}, maxtasks=10)
+
 # cProfile.run("c.runsync()")
 c.runsync()
-print(tracemalloc.get_traced_memory())
-
+# print(UrlParser().decode_url(
+# "https://www.thailandpostmart.com/product/1013460000224/ก/"))
+mem = tracemalloc.get_traced_memory()
+# print as mb
+print(f"Memory usage: {mem[0] / 10**6} MB")
+print(f"Memory peak: {mem[1] / 10**6} MB")
 # stopping the library
 tracemalloc.stop()
 # start = time.time()
-# a = check_duplicate('https://www.thailandpostmart.com/product/1013460000224/%E0%B8%8A%E0%B8%B5%E0%B8%97%E0%B8%A7%E0%B8%B1%E0%B8%99%E0%B8%AD%E0%B8%99%E0%B8%B8%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%A9%E0%B9%8C%E0%B8%A1%E0%B8%A3%E0%B8%94%E0%B8%81%E0%B9%84%E0%B8%97%E0%B8%A2%202562%20(1165)',
-#                     'https://www.thailandpostmart.com/product/1013460000501/%E0%B9%81%E0%B8%AA%E0%B8%95%E0%B8%A1%E0%B8%9B%E0%B9%8C%E0%B8%87%E0%B8%B2%E0%B8%99%E0%B9%80%E0%B8%9B%E0%B8%B4%E0%B8%94%E0%B8%AD%E0%B8%B2%E0%B8%84%E0%B8%B2%E0%B8%A3%E0%B8%99%E0%B8%A7%E0%B8%A1%E0%B8%B4%E0%B8%99%E0%B8%97%E0%B8%A3%E0%B8%9A%E0%B8%9E%E0%B8%B4%E0%B8%95%E0%B8%A3%2084%20%E0%B8%9E%E0%B8%A3%E0%B8%A3%E0%B8%A9%E0%B8%B2%20%E0%B8%A3%E0%B8%9E.%E0%B8%A8%E0%B8%B4%E0%B8%A3%E0%B8%B4%E0%B8%A3%E0%B8%B2%E0%B8%8A%20%E0%B9%81%E0%B8%9C%E0%B9%88%E0%B8%99%20(1211)')
+# a = check_duplicate('https://www.thailandpostmart.com/product/1013460000224/',
+#                     'https://www.thailandpostmart.com/product/1013460000224/')
 # stop = time.time()
 # print(stop - start)
 # # print typeof a
